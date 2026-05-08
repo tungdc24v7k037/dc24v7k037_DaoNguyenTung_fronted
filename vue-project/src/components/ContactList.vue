f456d980993b4ec0de2300add861f9fa1a4991f8
Dao Nguyen Tung <tungdc24v7k037@gmail.com>
2026-05-08 20:40:53 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ vue-project/src/components/ContactList.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    contacts: { type: Array, default: [] },
+    activeIndex: { type: Number, default: -1 },
+  },
+  emits: ["update:activeIndex"],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit("update:activeIndex", index);
+    },
+  },
+};
+</script>
+<template>
+  <ul class="list-group">
+    <li class="list-group-item"></li>
+  </ul>
+  :key="contact._id" v-for="(contact, index) in contacts" :class="{ active:
+  index === activeIndex }" @click="updateActiveIndex(index)"
+  {{ contact.name }}
+</template>
---
+++ vue-project/src/router/index.js
@@ -0,0 +1,13 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+];
+const router = createRouter({
+    routes,
+});
+export default router;
